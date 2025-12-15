3fb3ccb0fa6cf4b658c3d42b24834c81437bd240
ZhaoWei <wayne75235@gmail.com>
2025-12-15 13:24:49 +0800

    chore: configure db.js to connect to deploy database

--- Backend/db.js
+++ Backend/db.js
@@ -1,3 +1,15 @@
+// 區分本地開發/線上部署 .env
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'deploy' ? '.env.deploy' : '.env'
+});
+
+// 本地端開發，程式預設 NODE_ENV=development
+const NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(`Running in ${NODE_ENV} mode`);
+
+// 設定環境變數 -> TERMINAL打: $env:NODE_ENV="deploy"
+// 確認當前環境 -> echo $env:NODE_ENV
+
 // 建立連線池
 const { Pool } = require('pg');
 
