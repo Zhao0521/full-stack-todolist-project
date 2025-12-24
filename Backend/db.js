b2e32cf97017196629916fdf283f934e51e2980c
ZhaoWei <wayne75235@gmail.com>
2025-12-24 12:56:43 +0800

    chore: improve db.js comments for clarity

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, // process 是一個 全域物件，可控制 Node.js 程式執行環境的功能
     ssl: {
-        rejectUnauthorized: false // Render PostgreSQL 需要 SSL/TLS
+        rejectUnauthorized: false // Render PostgreSQL 忽略自簽憑證檢查
     }
 });
 
